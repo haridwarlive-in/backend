b7cc05f389461b7669094a2dadd9ddf3e47e6923
Manik Sharma <maniksharma.rke@gmail.com>
2025-03-09 11:48:21 +0530

    error resolved

--- src/controllers/newsController.ts
+++ src/controllers/newsController.ts
@@ -78,7 +78,8 @@ export const getNewsById = asyncHandler(async (req: Request, res: Response) => {
 });
 
 export const getNewsByTitle = asyncHandler(async (req: Request, res: Response) => {
-  const news = await News.find({urlTitle: req.params.title});
+  const decodedTitle = decodeURIComponent(req.params.title as string);
+  const news = await News.find({urlTitle: decodedTitle});
   if (news) {
     res.json(news);
   } else {
